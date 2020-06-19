34a2cc55a6cf0be5653a376954b8081a6769cfdd
Athanasios Vourvopoulos <a.vourvopoulos@gmail.com>
2020-06-19 19:49:05 +0100

    test 2

--- modules/paradigms/exampleMarkerBasedEpoching_test.py
+++ modules/paradigms/exampleMarkerBasedEpoching_test.py
@@ -12,22 +12,25 @@ if __name__ == '__main__':
 
     # initialize the pipeline
     lsl_marker_reception = lsl.LslReceive('type', 'Markers')
-    lsl_reception = lsl.LslReceive('name', 'LiveAmpSN-054207-0168')
-    #lsl_reception = lsl.LslReceive('name', 'openvibeSignal')
+    #lsl_reception = lsl.LslReceive('name', 'LiveAmpSN-054207-0168')
+    lsl_reception = lsl.LslReceive('name', 'openvibeSignal')
 
-    select = select.ChannelSelector(lsl_reception.output, 'index', [8, 25])
+    select = select.ChannelSelector(lsl_reception.output, 'index', [7, 24])
 
     butter_filter = filter.ButterFilter(select.output, 8, 12)
 
-    time_epoch = epoching.TimeBasedEpoching(butter_filter.output, 1, 0.5)
-
-    square_epoch = function.ApplyFunction(time_epoch.output, lambda x: x**2)
-
-    average_epoch = epoch_function.Average(square_epoch.output)
+    #time_epoch = epoching.TimeBasedEpoching(butter_filter.output, 1, 0.5)
+    #square_epoch = function.ApplyFunction(time_epoch.output, lambda x: x**2)
+    #average_epoch = epoch_function.Average(square_epoch.output)
 
     #log_epoch = function.ApplyFunction(time_epoch.output, lambda x: np.log1p(x))
 
-    baseline = epoching.StimulationBasedEpoching(average_epoch.output, lsl_marker_reception.output, 770, 0.125, 3)
+    baseline = epoching.StimulationBasedEpoching(butter_filter.output, lsl_marker_reception.output, 800, 0.025, 3)
+    #b_time_epoch = epoching.TimeBasedEpoching(baseline.output, 1, 0.025)
+    #b_square_epoch = function.ApplyFunction(b_time_epoch.output, lambda x: x**2)
+    #b_average_epoch = epoch_function.Average(b_square_epoch.output)
+
+    '''
     baseline_average = epoch_function.Average(baseline.output)
 
 
@@ -36,9 +39,9 @@ if __name__ == '__main__':
 
 
     relative_band = function.ApplyFunction(average_epoch.output, relative_alpha)
+    '''
 
-
-    lsl_send = lsl.LslSend(relative_band.output, 'relative_band')
+    lsl_send = lsl.LslSend(baseline.output, 'relative_band')
 
     # run the pipeline
     run()
