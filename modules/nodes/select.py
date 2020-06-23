c907ec4ffb0b15ecff0393833039339425943d9b
Simon <legeay.simon.sup@gmail.com>
2020-06-23 11:31:07 +0200

    add spatial filter

--- modules/nodes/select.py
+++ modules/nodes/select.py
@@ -1,5 +1,7 @@
 import sys
 
+import pandas as pd
+
 sys.path.append('.')
 sys.path.append('../..')
 
@@ -42,3 +44,53 @@ class ChannelSelector(Node):
                 self.output.set_from_df(chunk[self.selected])
             elif self.mode == 'index':
                 self.output.set_from_df(chunk.iloc[:, self.selected])
+
+
+class SpatialFilter(Node):
+    """Maps M inputs to N outputs by multiplying the each input vector with a matrix
+    usually used after a ChannelSelector
+    Attributes:
+     - output: output GroupOfPorts
+    Args:
+     - input: input port
+     - matrix: dictionnary with new channel name as keys and list of coefficients as values,
+       list must be of the same length as input.channels
+
+    example: SpatialFilter(input_port, matrix)
+    where matrix = {
+        'OC2': [4, 0, -1, 0],
+        'OC3': [0, -1, 2, 4]
+    }
+    """
+
+    def __init__(self, input_port, matrix):
+        Node.__init__(self, input_port)
+
+        self._matrix = matrix
+        self._channels = [*self._matrix.keys()]
+
+        # verify that the size of _matrix is correct
+        for chan in self._channels:
+            assert len(self._matrix[chan]) == len(self.input.channels)
+
+        self.output.set_parameters(
+            channels=self._channels,
+            frequency=self.input.frequency,
+            meta=self.input.meta)
+
+    def update(self):
+        for chunk in self.input:
+            df = pd.DataFrame([])
+            for chan in self._channels:
+                flag = True
+                for index, coef in enumerate(self._matrix[chan]):
+                    # print(f'{index} columns with coef {coef}')
+                    if flag:
+                        series = chunk.iloc[:, index] * coef
+                        flag = False
+                    else:
+                        series += chunk.iloc[:, index] * coef
+
+                df[chan] = series
+            self.output.set_from_df(df)
+
