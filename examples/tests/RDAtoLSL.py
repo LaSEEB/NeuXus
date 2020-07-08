fda76fa2b9a2e6ba7d3cdc32db0a1809c0105d84
Athanasios Vourvopoulos <a.vourvopoulos@gmail.com>
2020-07-08 19:13:25 +0100

    rda to lsltest

---
+++ examples/tests/RDAtoLSL.py
@@ -0,0 +1,20 @@
+import sys
+
+import numpy as np
+
+sys.path.append('..')
+
+from modules.nodes import *
+
+
+# receive RDA from Recorder or Recview
+#lsl_markers = io.LslReceive('type', 'Markers', data_type='marker')
+rda_reception = io.RdaReceive(rdaport=51244, host="192.168.1.132") #Recorder:51244 RecView:51254
+
+
+#send via LSL
+lsl_send = io.LslSend(rda_reception.output, 'rda_reception', 'EEG')
+
+
+#display
+#disp = display.Plot(rda_reception.output)
