a2b61ce3cafa4dc260f4a1a75826a86a885b8f20
Athanasios Vourvopoulos <a.vourvopoulos@gmail.com>
2020-07-12 11:36:36 +0100

    test stim/vis nodes

---
+++ examples/motor-imagery-simple/0_get-raw.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.path.append('..')
+import numpy as np
+
+from modules.nodes import (filter, io, select, epoching, epoch_function, store, generate, feature, function, display, stimulator)
+
+import datetime
+date = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+print (date)
+features_file = '../examples/motor-imagery-simple/Epochs_raw_'+date
+
+lsl_signal = generate.Generator('simulation', 32, 250)
+
+#stimulation and visualization
+lsl_markers = stimulator.Stimulator('../utils/stimulus/config_ov.xml') # load config
+graz_vis = display.Graz(lsl_markers.output) # visualize
+
+
+#temporal filtering
+butter_filter = filter.ButterFilter(lsl_signal.output, 8, 12)
+
+#left epochs
+left_epochs = epoching.StimulationBasedEpoching(butter_filter.output, lsl_markers.output, 769, 0, 4)
+#save features
+left_features = feature.FeatureAggregator(left_epochs.output, '1')
+tocsvL = store.ToCsv(left_features.output, features_file)
+
+#right epochs
+right_epochs = epoching.StimulationBasedEpoching(butter_filter.output, lsl_markers.output, 770, 0, 4)
+#save features
+right_features = feature.FeatureAggregator(right_epochs.output, '2')
+tocsvR = store.ToCsv(right_features.output, features_file)
