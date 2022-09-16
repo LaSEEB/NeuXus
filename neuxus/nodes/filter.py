256a33205be44c20eac2df0a9608946746ef9712
varjak <gmlcaetano@gmail.com>
2022-09-16 22:54:30 +0100

    added laplacian + updated numba requirement

--- neuxus/nodes/filter.py
+++ neuxus/nodes/filter.py
@@ -1,6 +1,9 @@
 from scipy import signal
 import numpy as np
 import pandas as pd
+import math
+from scipy.special import lpmv
+import mne
 
 from neuxus.node import Node
 
@@ -180,3 +183,115 @@ class DownSample(Node):
                 df = pd.DataFrame(df, index, self.input.channels)
                 self.output.set(df, index, self.input.channels)
                 self.persistent = to_keep
+
+class Laplacian(Node):
+
+    def __init__(self, input_port, loc=None, order=7, m=4, smoothing=10**(-5)):
+        Node.__init__(self, input_port)
+
+        self.output.set_parameters(
+            data_type=self.input.data_type,
+            channels=self.input.channels,
+            sampling_frequency=self.input.sampling_frequency,
+            meta=self.input.meta,
+            epoching_frequency=self.input.epoching_frequency
+        )
+
+        if isinstance(loc, mne.io.meas_info.Info):  # If read by Reader from .vhdr, head_size will be 0.085m
+            channames = loc.ch_names
+            chanlocs = loc['dig']
+        else:
+            montage = mne.channels.read_custom_montage(loc, head_size=0.095)
+            channames = montage.ch_names
+            chanlocs = montage.dig
+
+        chanlocs = [chanlocs[c] for chan1 in self.input.channels for c, chan2 in enumerate(channames) if chan1 == chan2]
+        if len(self.input.channels) != len(chanlocs):
+            raise Exception("Some data channels were not found in the provided locations!")
+
+        # # Check if channel names match channels from output
+        # for c, channame in enumerate(self.input.channels):
+        #     if channame != channames[c]:
+        #         raise Exception("Data channels do not match channels from provided locations")
+
+        # chanlocs = input_port.chanlocs[0]
+        X = np.zeros(len(chanlocs))
+        Y = np.zeros(len(chanlocs))
+        Z = np.zeros(len(chanlocs))
+        for c, chanloc in enumerate(chanlocs):
+            X[c] = chanloc['r'][0]
+            Y[c] = chanloc['r'][1]
+            Z[c] = chanloc['r'][2]
+
+        self.G, self.H = Laplacian.generate_laplacian_matrices(X, Y, Z, order, m)
+        self.smoothing = smoothing
+
+
+    @staticmethod
+    def cart2sph(x, y, z):
+        hxy = np.hypot(x, y)
+        r = np.hypot(hxy, z)
+        el = np.arctan2(z, hxy)
+        az = np.arctan2(y, x)
+        return az, el, r
+
+    @staticmethod
+    def generate_laplacian_matrices(X, Y, Z, order, m):
+        nchans = len(X)
+
+        # Set 'order' and 'm' in case of more than 100 electrodes
+        if nchans > 100:
+            order = 40
+            m = 3
+
+        # Calculate cosine distance
+        _, _, r = Laplacian.cart2sph(X, Y, Z)
+        maxr = max(r)
+        X = X / maxr
+        Y = Y / maxr
+        Z = Z / maxr
+        cosdist = np.zeros((nchans, nchans))
+        for i in range(nchans):
+            for j in range(i + 1, nchans):
+                cosdist[i, j] = 1 - ((X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2 + (Z[i] - Z[j]) ** 2) / 2
+
+        cosdist = cosdist + np.transpose(cosdist) + np.eye(nchans)
+
+        # Legendre polynomial
+        legpoly = np.zeros((order, nchans, nchans))
+        for i in range(order):
+            legpoly[i, :, :] = lpmv(0, i + 1, cosdist)
+
+        # Calculate G and H
+        orders = np.array(range(1, order + 1))
+        twoN1 = 2 * orders + 1
+        gdenom = (orders * (orders + 1)) ** m
+        hdenom = (orders * (orders + 1)) ** (m - 1)
+        G = np.zeros((nchans, nchans))
+        H = np.zeros((nchans, nchans))
+        for i in range(nchans):
+            for j in range(i, nchans):
+                g = 0
+                h = 0
+                for k in range(order):
+                    g = g + (twoN1[k] * legpoly[k, i, j]) / gdenom[k]
+                    h = h - (twoN1[k] * legpoly[k, i, j]) / hdenom[k]
+                G[i, j] = g / (4 * math.pi)
+                H[i, j] = -h / (4 * math.pi)
+
+        G = G + np.transpose(G)
+        H = H + np.transpose(H)
+        G = G - np.eye(nchans) * G[0, 0] / 2
+        H = H - np.eye(nchans) * H[0, 0] / 2
+        return G, H
+
+    def update(self):
+        for chunk in self.input:
+            data = np.asarray(chunk).copy()
+            nchans = len(data)
+            Gs = self.G + np.eye(nchans)*self.smoothing
+            GsinvS = np.sum(np.linalg.inv(Gs), axis=0, keepdims=True)
+            dataGs = np.transpose(np.linalg.lstsq(Gs, data)[0])
+            C = dataGs - (np.sum(dataGs, axis=1, keepdims=True) / np.sum(GsinvS)) @ GsinvS
+            lap = np.transpose(C @ np.transpose(self.H))
+            self.output.set(lap, chunk.index, columns=self.input.channels)
