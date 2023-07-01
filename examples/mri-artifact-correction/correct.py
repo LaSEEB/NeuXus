ed5d8d5a588c6346353b92e4e9fb276ba648e04a
varjak <33642154+varjak@users.noreply.github.com>
2023-07-01 21:42:20 +0100

    Added ECG bandpass filter

--- examples/mri-artifact-correction/correct.py
+++ examples/mri-artifact-correction/correct.py
@@ -7,8 +7,11 @@ weight_path = r'weights-input-500.pkl'
 signal = read.Reader(data_path)
 
 signal_ga = correct.GA(signal.output, marker_input_port=signal.marker_output, start_marker='Response/R128')  # 'Response/R128' is the marker of the start of every MRI volume (in case the data is read from a Brain Vision file; in case it's streamed by Brain Vision Recorder, it is 'R128')
-signal_dw = filter.DownSample(signal_ga.output, int(5000 / 250))
-signal_pa = correct.PA(signal_ds.output, weight_path, marker_input_port=signal_ga.marker_output, start_marker='Start of GA subtraction', stride=50)
+signal_ds = filter.DownSample(signal_ga.output, int(5000 / 250))
+signal_ds_ecg = select.ChannelSelector(signal_ds.output, 'name', ['ECG'])
+signal_ds_ecg_fi = filter.ButterFilter(signal_ds_ecg.output,  0.5, 30)
+signal_ds_fi = select.ChannelUpdater(signal_ds.output, signal_ds_ecg_fi.output)
+signal_pa = correct.PA(signal_ds_fi.output, weight_path, marker_input_port=signal_ga.marker_output, start_marker='Start of GA subtraction', stride=50)
 
 signal_m_lsl = io.LslSend(signal.marker_output, 'marker', type='Markers', format='string')
 signal_pa_lsl = io.LslSend(signal_pa.output, 'signal_pa', type='EEG')
